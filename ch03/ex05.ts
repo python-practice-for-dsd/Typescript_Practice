21c833a6aeea51dd723f3d71cc7e4e803506c9ce
shimmy0619 <sausupo0619@yahoo.co.jp>
2020-04-10 13:53:35 +0900

    ex04,05を追加

--- ch03/ex05.ts
+++ ch03/ex05.ts
@@ -22,3 +22,31 @@ const v5: number = f3();
 // エラー例
 const v6: number = f1();
 */
+
+// conditional type
+// Aがundefinedなら(arg?: A) => R、そうでなければ(arg: A) => R
+type Func<A, R> = undefined extends A ? (arg?: A) => R : (arg: A) => R;
+
+// 使用例
+const f1: Func<number, number> = num => num + 10;
+const v1: number = f1(10);
+
+// 下記2つが可能
+// 明示的にundefinedを指定する
+// 引数無し
+const f2: Func<undefined, number> = () => 0;
+const v2: number = f2();
+const v3: number = f2(undefined);
+
+// 下記3つが可能
+// numberを明示的に指定する
+// undefinedを明示的に指定する
+// 引数無し
+const f3: Func<number | undefined, number> = num => (num || 0) + 10;
+const v4: number = f3(123);
+const v5: number = f3();
+const v7: number = f3(undefined); // 追加
+
+// エラー例
+// Aがundefined以外のときは引数の省略は許さない
+const v6: number = f1();
